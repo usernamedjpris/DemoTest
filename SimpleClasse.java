ebfebceb78a3eb7d19af4983f9651cb3e1ce668a
usernamedjpris <jeremie.gantet@gmail.com>
2020-12-09 10:42:04 +0100

    ajout de la classe SimpleClass

---
+++ SimpleClasse.java
@@ -0,0 +1,14 @@
+public class SimpleClasse {
+	public int somme(int a, int b){
+		return a+b;
+	}
+
+	public static void main(String[] args) {
+		SimpleClasse sc = new SimpleClasse();
+		int a = 3;
+		int b = 5;
+		System.out.println("La somme de "+a+" et "+b+" est : "+sc.somme(a,b));	
+
+	}
+}
+
